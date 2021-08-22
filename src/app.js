f0f249a035a442a31b2807e265855bf96f868ba8
Supelion <mutayyabtariq9@gmail.com>
2021-08-22 12:23:06 +0300

    Added all the files | v0.0.1

---
+++ src/app.js
@@ -0,0 +1,57 @@
+const inputElement = document.querySelector("#inputEl");
+const checkBtn = document.querySelector("#checkBtn");
+const fkdrEl = document.querySelector("#fkdr");
+const indexEl = document.querySelector("#index");
+const starsEl = document.querySelector("#stars");
+const finalDeathsEl = document.querySelector("#finalKills");
+const finalKillsEl = document.querySelector("#finalDeaths");
+const errorEl = document.querySelector("#errorEl");
+const loadingEl = document.querySelector("#loadingStatsEl");
+checkBtn.addEventListener("click", getStats);
+
+async function getStats() {
+    loadingStatsEl.textContent = "Loading Stats...";
+
+    fkdrEl.textContent = "";
+    indexEl.textContent = "";
+    starsEl.textContent = "";
+    finalKillsEl.textContent = "";
+    finalDeathsEl.textContent = "";
+
+    const inputVal = inputElement.value;
+
+    if (inputVal.length < 1) {
+        errorEl.textContent = "Please enter a player!";
+        return
+    };
+
+    try {
+        const apiRaw = await fetch(`https://api.slothpixel.me/api/players/${inputVal}`, {method: "GET"});
+        const apiData = await apiRaw.json();
+
+        let stars = 0;
+        let finalDeaths = 0;
+        let finalKills = 0;
+        let fkdr = 0;
+        let index = 0
+
+        stars += apiData.stats.BedWars.level;
+        finalDeaths += apiData.stats.BedWars.final_deaths;
+        finalKills += apiData.stats.BedWars.final_kills;
+        fkdr += parseFloat(((finalKills) / (finalDeaths)).toFixed(1));
+        index += parseFloat(((stars * fkdr ** 2) / 10).toFixed(1));
+
+        inputVal.textContent = "";
+        errorEl.textContent = "";
+
+        fkdrEl.textContent = `FKDR: ${fkdr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+        indexEl.textContent = `Index: ${index.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+        starsEl.textContent = `Stars: ${stars.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+        finalKillsEl.textContent = `Final Kills: ${finalKills.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+        finalDeathsEl.textContent = `Final Deaths: ${finalDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
+        loadingStatsEl.textContent = "";
+
+    } catch (err) {
+        errorEl.textContent = "Invalid Player specified!";
+    };
+};
---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    icon: path.join(__dirname, "./assets/logo.png"),
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    backgroundColor: "#ffffff",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
